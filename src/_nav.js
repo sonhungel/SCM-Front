00e12d24d17bd350cb2b156baeae6ca521d27b38
sonhungel <17521310@gm.uit.edu.vn>
2021-10-24 01:11:14 +0700

    create new login page

--- src/_nav.js
+++ src/_nav.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import CIcon from '@coreui/icons-react'
+import React from "react";
+import CIcon from "@coreui/icons-react";
 import {
   cilBell,
   cilChartPie,
@@ -10,68 +10,89 @@ import {
   cilPuzzle,
   cilSpeedometer,
   cilStar,
-} from '@coreui/icons'
-import { CNavItem} from '@coreui/react'
+} from "@coreui/icons";
+import { CNavGroup, CNavItem, CNavTitle } from "@coreui/react";
 
 const _nav = [
   {
     component: CNavItem,
-    name: 'Tổng quan',
-    to: '/dashboard',
+    name: "Tổng quan",
+    to: "/dashboard",
     icon: <CIcon icon={cilSpeedometer} customClassName="nav-icon" />,
     badge: {
-      color: 'info'
+      color: "info",
     },
   },
   {
     component: CNavItem,
-    name: 'Đơn hàng',
-    to: '/theme/colors',
+    name: "Đơn hàng",
+    to: "/theme/colors",
     icon: <CIcon icon={cilDrop} customClassName="nav-icon" />,
   },
   {
     component: CNavItem,
-    name: 'Hàng hoá',
-    to: '/theme/typography',
+    name: "Hàng hoá",
+    to: "/theme/typography",
     icon: <CIcon icon={cilPencil} customClassName="nav-icon" />,
   },
   {
     component: CNavItem,
-    name: 'Đối tác',
-    to: '/base',
+    name: "Đối tác",
+    to: "/base",
     icon: <CIcon icon={cilPuzzle} customClassName="nav-icon" />,
   },
   {
     component: CNavItem,
-    name: 'Nhập hàng',
-    to: '/buttons',
+    name: "Nhập hàng",
+    to: "/buttons",
     icon: <CIcon icon={cilCursor} customClassName="nav-icon" />,
   },
   {
     component: CNavItem,
-    name: 'Tồn kho',
-    to: '/charts',
+    name: "Tồn kho",
+    to: "/charts",
     icon: <CIcon icon={cilNotes} customClassName="nav-icon" />,
   },
   {
     component: CNavItem,
-    name: 'Doanh số',
-    to: '/chart',
+    name: "Doanh số",
+    to: "/chart",
     icon: <CIcon icon={cilChartPie} customClassName="nav-icon" />,
   },
   {
     component: CNavItem,
-    name: 'Lợi nhuận',
-    to: '/chats',
+    name: "Lợi nhuận",
+    to: "/chats",
     icon: <CIcon icon={cilStar} customClassName="nav-icon" />,
   },
   {
     component: CNavItem,
-    name: 'Thiết lập',
-    to: '/carts',
+    name: "Thiết lập",
+    to: "/carts",
     icon: <CIcon icon={cilBell} customClassName="nav-icon" />,
   },
+  {
+    component: CNavGroup,
+    name: "Pages",
+    icon: <CIcon icon={cilStar} customClassName="nav-icon" />,
+    items: [
+      {
+        component: CNavItem,
+        name: "Login",
+        to: "/login",
+      },
+      {
+        component: CNavItem,
+        name: "Error 404",
+        to: "/404",
+      },
+      {
+        component: CNavItem,
+        name: "Error 500",
+        to: "/500",
+      },
+    ],
+  },
+];
 
-]
-
-export default _nav
+export default _nav;
--- src/components/AppFooter.js
+++ src/components/AppFooter.js
@@ -1,23 +1,15 @@
-import React from 'react'
-import { CFooter } from '@coreui/react'
+import React from "react";
+import { CFooter } from "@coreui/react";
 
 const AppFooter = () => {
   return (
     <CFooter>
       <div>
-        <a href="https://coreui.io" target="_blank" rel="noopener noreferrer">
-          CoreUI
-        </a>
-        <span className="ms-1">&copy; 2021 creativeLabs.</span>
-      </div>
-      <div className="ms-auto">
-        <span className="me-1">Powered by</span>
-        <a href="https://coreui.io/react" target="_blank" rel="noopener noreferrer">
-          CoreUI for React
-        </a>
+        SCM system
+        <span className="ms-1">&copy; 2021 UIT</span>
       </div>
     </CFooter>
-  )
-}
+  );
+};
 
-export default React.memo(AppFooter)
+export default React.memo(AppFooter);
