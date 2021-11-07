54ee29eabca109dcf6420ff5173d7842e3834b25
Quan Nguyen <rongkill103@gmail.com>
2021-11-07 23:15:24 +0700

    update inventory

--- src/Views/Inventory/Inventory.js
+++ src/Views/Inventory/Inventory.js
@@ -1,26 +1,60 @@
 import React, { Fragment } from "react";
 import {Button, Col, Form, Row, Table, Container} from "react-bootstrap";
+import { useForm } from "react-hook-form";
 import AppHeader from "../../Layout/AppHeader/";
 import AppSidebar from "../../Layout/AppSidebar/";
 import AppFooter from "../../Layout/AppFooter/";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./Inventory.css"
 const Inventory = () => {
-  return (
+    const { register, control, handleSubmit, setValue, getValues, reset } = useForm({ defaultValues: {}, shouldUnregister: false });
+
+    const onSubmitForm = (data) => {
+        
+    };
+
+    const resetForm = () => {
+        
+    }
+
+    return (
     <Fragment>
         <AppHeader />
         <div className="app-main">
         <AppSidebar />
         <div className="app-main__outer">
             <div className="app-main__inner">
-            <Button class="inventory-btnnnn" variant="primary" type="submit">test add</Button>
+            <Container fluid className={"pl-0 pr-5 pt-4"}>
+            <div>Sản phẩm</div>
+                <Form onSubmit={handleSubmit(onSubmitForm)} className={"pt-4"}>
+                    <Form.Row>
+                        <Col xl={6} className={"text-left"}>
+                        <Button variant="primary" onClick={resetForm}>Tạo</Button>
+                        </Col>
+                        <Col xl={3}>
+                            <Form.Control type="input" />
+                        </Col>
+                        <Col xl={2} className={"text-right"}>
+                            <Button variant="primary" type="submit">
+                                Tìm kiếm
+                            </Button>
+                        </Col>
+                        <Col xl={1} className={"text-left"}>
+                            <Button variant="secondary" onClick={resetForm}>
+                                Tải lại
+                            </Button>
+                        </Col>
+                    </Form.Row>
+                </Form>
+            <p />
             <Table striped bordered hover>
                 <thead>
                     <tr>
                         <th>#</th>
-                        <th>First Name</th>
-                        <th>Last Name</th>
-                        <th>Username</th>
+                        <th>Mã hàng</th>
+                        <th>Tên hàng</th>
+                        <th>Đơn giá</th>
+                        <th>Tồn kho</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -29,20 +63,11 @@ const Inventory = () => {
                         <td>Mark</td>
                         <td>Otto</td>
                         <td>@mdo</td>
-                    </tr>
-                    <tr>
-                        <td>2</td>
-                        <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
-                    </tr>
-                        <tr>
-                        <td>3</td>
-                        <td colSpan="2">Larry the Bird</td>
-                        <td>@twitter</td>
+                        <td>@mdo</td>
                     </tr>
                 </tbody>
             </Table>
+            </Container>
             </div>
             <AppFooter />
         </div>
